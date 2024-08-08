01f93665d6a3d76ffaa2ada94999d3f3a2bb49ad
Hunain Ali <119546006+HunainBaloch@users.noreply.github.com>
2024-08-08 21:58:41 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import panda as pd_df
+import pandas as pd_df
 # Write directly to the app
 st.title("Example Streamlit App :cup_with_straw:")
 st.write(
